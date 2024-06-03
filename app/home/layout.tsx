6b007b394f4a4f42eea114713d1936597cab0438
VillageR88 <villager88pl@gmail.com>
2024-06-03 18:21:25 +0200

    layout improved and initial logics of step switch
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,9 +1,21 @@
-import { ReactNode } from 'react';
+'use client';
+import { ReactNode, useState } from 'react';
 import Image from 'next/image';
 import imageSidebarDesktop from '@/public/assets/images/bg-sidebar-desktop.svg';
 
 export default function LayoutHome({ children }: { children: ReactNode }) {
-  const items = ['YOUR INFO', 'SELECT PLAN', 'ADD-ONS', 'SUMMARY'];
+  enum Steps {
+    YOUR_INFO = 'YOUR INFO',
+    SELECT_PLAN = 'SELECT PLAN',
+    ADD_ONS = 'ADD-ONS',
+    SUMMARY = 'SUMMARY',
+  }
+  const circleDisplay = {
+    normal: 'text-white border-white',
+    selected: 'text-[#022959] bg-[#BEE2FD] border-[#BEE2FD]',
+  };
+  const [currentStep, setCurrentStep] = useState<Steps>(Steps.YOUR_INFO);
+  const items = [Steps.YOUR_INFO, Steps.SELECT_PLAN, Steps.ADD_ONS, Steps.SUMMARY];
   const titles = {
     step: 'STEP ',
     buttonPrevious: 'Go Back',
@@ -24,7 +36,9 @@ export default function LayoutHome({ children }: { children: ReactNode }) {
           <ul className="relative z-10 mt-[40px] flex h-[228px] flex-col gap-[32px] pl-[32px]">
             {Object.values(items).map((item, index) => (
               <li key={index} className="flex h-[33px] items-center gap-[16px]">
-                <div className="flex size-[33px] items-center justify-center rounded-full text-[14px] font-bold text-white outline outline-1 outline-white">
+                <div
+                  className={`${currentStep === item ? circleDisplay.selected : circleDisplay.normal} flex size-[33px] items-center justify-center rounded-full border text-[14px] font-bold`}
+                >
                   {index + 1}
                 </div>
                 <div className="flex flex-col">
@@ -38,10 +52,28 @@ export default function LayoutHome({ children }: { children: ReactNode }) {
         <div className="flex size-full flex-col pb-[16px]">
           <div className="size-full">{children}</div>
           <div className="flex h-[48px] w-full items-center justify-between">
-            <button type="button" className="h-[18px] w-[60px] text-[#9699AA]">
+            <button
+              onClick={() => {
+                const currentIndex = items.indexOf(currentStep);
+                if (currentIndex > 0) {
+                  setCurrentStep(items[currentIndex - 1]);
+                }
+              }}
+              type="button"
+              className={`h-[18px] w-[60px] text-[#9699AA] ${items[0] === currentStep ? 'invisible' : 'visible'}`}
+            >
               {titles.buttonPrevious}
             </button>
-            <button type="button" className="h-[48px] w-[123px] rounded-[8px] bg-[#022959] text-white">
+            <button
+              onClick={() => {
+                const currentIndex = items.indexOf(currentStep);
+                if (currentIndex < items.length - 1) {
+                  setCurrentStep(items[currentIndex + 1]);
+                }
+              }}
+              type="button"
+              className="h-[48px] w-[123px] rounded-[8px] bg-[#022959] text-white"
+            >
               {titles.buttonNext}
             </button>
           </div>
