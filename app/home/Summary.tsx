2d2b9fbe40723130e8e52f81435cd74ead28cf92
VillageR88 <villager88pl@gmail.com>
2024-06-05 22:47:55 +0200

    summary calculations improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Summary.tsx
+++ app/home/Summary.tsx
@@ -43,6 +43,8 @@ export default function Summary() {
   };
 
   const billingText = billing ? items.billing.yearly : items.billing.monthly;
+  const anyChecked = addons.onlineService.checked || addons.largerStorage.checked || addons.customizableProfile.checked;
+  const totalTitle = 'Total ' + (billing ? '(per year)' : '(per month)');
   return (
     <div
       ref={summaryRef}
@@ -52,53 +54,60 @@ export default function Summary() {
         <h1>{items.title}</h1>
         <p>{items.description}</p>
       </header>
-      <div className="flex flex-col gap-[24px] rounded-[8px] bg-[#F8F9FF] px-[24px] pb-[24px] pt-[16px]">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-[7px]">
-            <h2>{plan && plan[0][0].toUpperCase() + plan[0].slice(1) + billingText.toString()}</h2>
-            <button
-              onClick={() => {
-                if (!summaryRef.current || !selectPlanRef.current) return;
-                summaryRef.current.classList.remove('selected');
-                summaryRef.current.classList.add('hidden');
-                summaryRef.current.classList.remove('flex');
-                selectPlanRef.current.classList.add('selected');
-                selectPlanRef.current.classList.remove('hidden');
-                selectPlanRef.current.classList.add('flex');
-              }}
-              type="button"
-              className="w-fit text-[14px] leading-[20px] text-[#9699AA] underline underline-offset-2"
-            >
-              Change
-            </button>
+      <div className="flex flex-col gap-[24px]">
+        <div className="flex flex-col gap-[24px] rounded-[8px] bg-[#F8F9FF] px-[24px] pb-[24px] pt-[16px]">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-[7px]">
+              <h2>{plan && plan[0][0].toUpperCase() + plan[0].slice(1) + billingText.toString()}</h2>
+              <button
+                onClick={() => {
+                  if (!summaryRef.current || !selectPlanRef.current) return;
+                  summaryRef.current.classList.remove('selected');
+                  summaryRef.current.classList.add('hidden');
+                  summaryRef.current.classList.remove('flex');
+                  selectPlanRef.current.classList.add('selected');
+                  selectPlanRef.current.classList.remove('hidden');
+                  selectPlanRef.current.classList.add('flex');
+                }}
+                type="button"
+                className="w-fit text-[14px] leading-[20px] text-[#9699AA] underline underline-offset-2"
+              >
+                Change
+              </button>
+            </div>
+            <p className="text-[16px] font-bold text-[#022959]">
+              {costFormatted({
+                cost: plan?.[1],
+                billing,
+              })}
+            </p>
           </div>
-          <p className="text-[16px] font-bold text-[#022959]">
-            {costFormatted({
-              cost: plan?.[1],
-              billing,
-            })}
-          </p>
+          {anyChecked && (
+            <div className="flex flex-col gap-[16px] text-[14px] text-[#9699AA]">
+              <div className="h-px w-full bg-[#9699AA]/20"></div>
+              {Object.keys(items.addons).map((key) => {
+                const addon = items.addons[key as keyof typeof items.addons];
+                return (
+                  addon.condition && (
+                    <div key={key} className="flex justify-between">
+                      <p>{addon.title}</p>
+                      <p className="text-[#022959]">
+                        {costFormatted({
+                          cost: Object.entries(addons).filter((x) => x[0] === key)[0][1].cost,
+                          billing: billing,
+                          prefixWithPlus: true,
+                        })}
+                      </p>
+                    </div>
+                  )
+                );
+              })}
+            </div>
+          )}
         </div>
-        {(addons.onlineService.checked || addons.largerStorage.checked || addons.customizableProfile.checked) && (
-          <div className="flex flex-col gap-[16px] text-[14px] text-[#9699AA]">
-            <div className="h-px w-full bg-[#9699AA]/20"></div>
-            {Object.keys(items.addons).map((key) => {
-              const addon = items.addons[key as keyof typeof items.addons];
-              return (
-                addon.condition && (
-                  <div key={key} className="flex justify-between">
-                    <p>{addon.title}</p>
-                    <p className="text-[#022959]">
-                      {costFormatted({
-                        cost: Object.entries(addons).filter((x) => x[0] === key)[0][1].cost,
-                        billing: billing,
-                        prefixWithPlus: true,
-                      })}
-                    </p>
-                  </div>
-                )
-              );
-            })}
+        {anyChecked && (
+          <div className="flex justify-between px-[24px]">
+            <p>{totalTitle}</p>
           </div>
         )}
       </div>
